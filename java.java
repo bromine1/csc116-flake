7e8395ba7a85f2f75dcd53c8edbae36e724487fd
bromine1 <rwstauffer72@gmail.com>
2023-08-18 08:22:12 -0400

    add java so the flake can see it

---
+++ java.java
@@ -0,0 +1,9 @@
+// Your First Program
+import java.io.*;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+        System.out.println("YOOOOO");
+    }
+}
